67445278c425a3fcc3e6f4df4a8dcce1aeb3a826
jana <rathan.kparam@gmail.com>
2023-04-29 12:24:08 +0800

    Init plan-finder

---
+++ plan-finder/src/main/java/org/jana/planfinder/PlanFinderApplication.java
@@ -0,0 +1,13 @@
+package org.jana.planfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanFinderApplication.class, args);
+	}
+
+}
---
+++ plan-finder/src/test/java/org/jana/planfinder/PlanFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jana.planfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
