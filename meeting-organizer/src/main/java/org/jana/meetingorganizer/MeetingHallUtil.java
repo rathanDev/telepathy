5d8e0da5a7fc301e846840e716ce2e7eac0e364c
jana <rathan.kparam@gmail.com>
2023-04-29 17:45:34 +0800

    Create meeting hall finder

---
+++ meeting-organizer/src/main/java/org/jana/meetingorganizer/MeetingHallUtil.java
@@ -0,0 +1,37 @@
+package org.jana.meetingorganizer;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MeetingHallUtil {
+
+    public static List<Slot> convertToSlots(List<String> lines) {
+        return lines.stream().map(MeetingHallUtil::convertToSlot).collect(Collectors.toList());
+    }
+
+    private static Slot convertToSlot(String str) {
+        String[] splits = str.split("-");
+        String fromTimeStr = splits[0];
+        String toTimeStr = splits[1];
+
+        String[] fromTimeStrSplits = fromTimeStr.split(":");
+        int fromTimeHour = Integer.parseInt(fromTimeStrSplits[0]);
+        int fromTimeMinute = Integer.parseInt(fromTimeStrSplits[1]);
+
+        String[] toTimeStrSplits = toTimeStr.split(":");
+        int toTimeHour = Integer.parseInt(toTimeStrSplits[0]);
+        int toTimeMinute = Integer.parseInt(toTimeStrSplits[1]);
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime from = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), fromTimeHour, fromTimeMinute);
+        LocalDateTime to = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), toTimeHour, toTimeMinute);
+        int duration = (int) ChronoUnit.MINUTES.between(from, to) - 1; // bcz of the examples
+
+        int start = (from.getHour() * 60) + from.getMinute();
+
+        return new Slot(start, duration);
+    }
+
+}
--- meeting-organizer/src/main/java/org/jana/meetingorganizer/MeetingOrganizerApplication.java
+++ meeting-organizer/src/main/java/org/jana/meetingorganizer/MeetingOrganizerApplication.java
@@ -1,10 +1,5 @@
 package org.jana.meetingorganizer;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,74 +7,31 @@ import java.util.Map;
 
 public class MeetingOrganizerApplication {
 
-    public static final int SIZE = 24 * 60;
+    public static final int OCCUPATION_SIZE = 24 * 60;
     public static List<Integer> hallIds = new ArrayList<>();
     public static Map<Integer, boolean[]> hallMap = new HashMap<>();
 
     public static void main(String[] args) {
+        String filePath = args[0];
+        List<String> lines = FileReaderUtil.readFile(filePath);
 
-        String filePath = "C:\\VProjects\\telepathy\\meeting-organizer\\src\\main\\resources\\input2.txt";
-        List<String> lines = readFile(filePath);
+        List<Slot> slots = MeetingHallUtil.convertToSlots(lines);
 
-        for (String str : lines) {
-            Slot slot = convertToSlot(str);
+        for (Slot slot : slots) {
             int hallId = findHall(slot);
             bookHall(hallId, slot);
         }
         int count = findCount();
-        System.out.println("count = " + count);
+        System.out.println(count);
     }
 
-    private static List<String> readFile(String filePath) {
-        List<String> lines = new ArrayList<>();
-        try {
-            String content = Files.readString(Paths.get(filePath));
-            String[] splits = content.split("\n");
-            for (String line : splits) {
-                String trimmed = line.replaceAll("\r", "").trim();
-                if (!trimmed.isEmpty()) {
-                    lines.add(trimmed);
-                }
-            }
-        } catch (IOException e) {
-
-        }
-        return lines;
-    }
-
-    private static int findCount() {
-        return hallMap.keySet().size();
-    }
-
-    private static Slot convertToSlot(String str) {
-        String[] splits = str.split("-");
-        String fromTimeStr = splits[0];
-        String toTimeStr = splits[1];
-
-        String[] fromTimeStrSplits = fromTimeStr.split(":");
-        int fromTimeHour = Integer.parseInt(fromTimeStrSplits[0]);
-        int fromTimeMinute = Integer.parseInt(fromTimeStrSplits[1]);
-
-        String[] toTimeStrSplits = toTimeStr.split(":");
-        int toTimeHour = Integer.parseInt(toTimeStrSplits[0]);
-        int toTimeMinute = Integer.parseInt(toTimeStrSplits[1]);
-
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime from = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), fromTimeHour, fromTimeMinute);
-        LocalDateTime to = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), toTimeHour, toTimeMinute);
-        int duration = (int) ChronoUnit.MINUTES.between(from, to) - 1;
-
-        int start = (from.getHour() * 60) + from.getMinute();
-
-        return new Slot(start, duration);
-    }
 
     private static int findHall(Slot slot) {
         if (hallIds.isEmpty()) {
             int hallId = 0;
-            boolean[] occupations = new boolean[SIZE];
-            hallMap.put(hallId, occupations);
+            boolean[] occupations = new boolean[OCCUPATION_SIZE];
             hallIds.add(hallId);
+            hallMap.put(hallId, occupations);
             return hallId;
         }
 
@@ -100,9 +52,9 @@ public class MeetingOrganizerApplication {
         }
 
         int hallId = hallIds.size();
-        boolean[] occupations = new boolean[SIZE];
-        hallMap.put(hallId, occupations);
+        boolean[] occupations = new boolean[OCCUPATION_SIZE];
         hallIds.add(hallId);
+        hallMap.put(hallId, occupations);
         return hallId;
     }
 
@@ -114,6 +66,10 @@ public class MeetingOrganizerApplication {
             availability[i] = true;
         }
         hallMap.put(hallId, availability);
+    }
+
+    private static int findCount() {
+        return hallMap.keySet().size();
     }
 
 }
